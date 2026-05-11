2758a34d9089c1c741714a4cbb40e68d8f7a4ca4
Amit Shirasao <amit.shirasao@gmail.com>
2026-05-12 05:20:28 +0530

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Portfolio } from './portfolio/portfolio';
+import { AboutMe } from './about-me/about-me';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'portfolio', component: Portfolio },
+  { path: 'about-me', component: AboutMe },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
